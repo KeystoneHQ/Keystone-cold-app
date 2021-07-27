c66a3e58cea6d448e2607f459079790d6caddc71
final_statics <finalstatics@gmail.com>
2021-07-27 11:34:03 +0800

    optimize array abi parsing

--- coinlib/src/main/java/com/keystone/coinlib/coins/ETH/CanonicalValues/CanonicalArrayValue.java
+++ coinlib/src/main/java/com/keystone/coinlib/coins/ETH/CanonicalValues/CanonicalArrayValue.java
@@ -8,44 +8,29 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class CanonicalArrayValue extends CanonicalValue {
-    int count;
     CanonicalValue canonicalValue;
 
     CanonicalArrayValue(ABIType abiType) {
         super(abiType);
         ArrayType arrayType = (ArrayType) abiType;
-        count = getCount(abiType.getCanonicalType());
         canonicalValue = CanonicalValue.getCanonicalValue(arrayType.getElementType());
     }
 
     @Override
     public void resolveValueToJSONObject(Object value, JSONObject jsonObject) throws JSONException {
-        if (count == 1) {
-            JSONArray jsonArray = new JSONArray();
-            Object[] objects = (Object[]) value;
-            for (int i = 0; i < objects.length; i++) {
-                Object subValue = objects[i];
-                JSONObject subObject = new JSONObject();
-                canonicalValue.resolveValueToJSONObject(subValue, subObject);
-                jsonArray.put(subObject.optString("value"));
-            }
-            jsonObject.put("value", jsonArray);
-        } else if (count == 2) {
-            JSONArray jsonArray = new JSONArray();
-            Object[][] objects = (Object[][]) value;
-            for (int i = 0; i < objects.length; i++) {
-                Object[] subValues = objects[i];
-                JSONObject subObject = new JSONObject();
-                canonicalValue.resolveValueToJSONObject(subValues, subObject);
-                jsonArray.put(subObject.optJSONArray("value"));
-            }
-            jsonObject.put("value", jsonArray);
+        JSONArray jsonArray = new JSONArray();
+        JSONArray valueArray;
+        if (value instanceof JSONArray) {
+            valueArray = (JSONArray)value;
+        } else {
+            valueArray = new JSONArray(value);
         }
-    }
-
-    public static int getCount(String canonicalType) {
-        int length = canonicalType.length();
-        canonicalType = canonicalType.replace("]", "");
-        return length - canonicalType.length();
+        for (int i = 0; i < valueArray.length(); i++) {
+            Object subValues = valueArray.get(i);
+            JSONObject subObject = new JSONObject();
+            canonicalValue.resolveValueToJSONObject(subValues, subObject);
+            jsonArray.put(subObject.opt("value"));
+        }
+        jsonObject.put("value", jsonArray);
     }
 }
