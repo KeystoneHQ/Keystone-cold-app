d12a2cc868d98baafc87130b2be164324f3b2fa2
final_statics <finalstatics@gmail.com>
2021-10-22 14:08:38 +0800

    restore commit 5b55c190887463a56f5b24faa285c45127e8e5f9

--- app/src/main/java/com/keystone/cold/ui/views/AuthenticateModal.java
+++ app/src/main/java/com/keystone/cold/ui/views/AuthenticateModal.java
@@ -84,6 +84,13 @@ public class AuthenticateModal {
                                         OnVerify onVerify,
                                         PasswordInputModalBinding binding,
                                         ModalDialog dialog) {
+        FingerprintManager.CryptoObject cryptoObject = initCryptoObject();
+        if (cryptoObject == null) {
+            binding.fingerprintLayout.setVisibility(View.GONE);
+            binding.passwordLayout.setVisibility(View.VISIBLE);
+            Keyboard.show(activity, binding.input);
+            return;
+        }
         FingerprintKit fpKit = new FingerprintKit(activity);
         fpKit.startVerify(new VerifyListener() {
             @Override
@@ -95,12 +102,13 @@ public class AuthenticateModal {
 
             @Override
             public void onAuthenticationSucceeded(FingerprintManager.CryptoObject cryptoObject) {
+                Signature signature = cryptoObject.getSignature();
                 dialog.dismiss();
                 if (onVerify != null) {
-                    onVerify.onVerify(new OnVerify.VerifyToken(null, getSignature()));
+                    onVerify.onVerify(new OnVerify.VerifyToken(null, signature));
                 }
             }
-        }, null);
+        }, initCryptoObject());
         binding.fingerprintLayout.setVisibility(View.VISIBLE);
         binding.fingerprintLayout.setTag(fpKit);
         binding.passwordLayout.setVisibility(View.GONE);
@@ -112,18 +120,19 @@ public class AuthenticateModal {
         });
     }
 
-    private static Signature getSignature() {
+    private static FingerprintManager.CryptoObject initCryptoObject() {
         Signature signature;
         try {
             signature = Signature.getInstance("SHA256withECDSA");
-            // since setUserAuthenticationRequired is true, need to verify the fingerprint before query
             PrivateKey key = (PrivateKey) KeyStoreUtil.prepareKeyStore().getKey(SECP256R1,null);
             signature.initSign(key);
         } catch (NoSuchAlgorithmException | UnrecoverableKeyException | KeyStoreException | InvalidKeyException e) {
             e.printStackTrace();
             return null;
         }
-        return signature;
+        FingerprintManager.CryptoObject cryptoObject = null;
+        cryptoObject = new FingerprintManager.CryptoObject(signature);
+        return cryptoObject;
     }
 
     private static void initPassword(AppCompatActivity activity,
