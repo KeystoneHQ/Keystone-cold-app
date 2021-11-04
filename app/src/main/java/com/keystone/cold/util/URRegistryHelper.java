324971ef30e2605e8c64bfc5ecdd467b1e4a1aa3
final_statics <finalstatics@gmail.com>
2021-11-04 15:37:22 +0800

    compatible export matamask

--- app/src/main/java/com/keystone/cold/util/URRegistryHelper.java
+++ app/src/main/java/com/keystone/cold/util/URRegistryHelper.java
@@ -1,8 +1,10 @@
 package com.keystone.cold.util;
 
+import com.keystone.coinlib.accounts.ETHAccount;
 import com.keystone.coinlib.accounts.ExtendedPublicKey;
 import com.keystone.cold.callables.GetExtendedPublicKeyCallable;
 import com.keystone.cold.callables.GetMasterFingerprintCallable;
+import com.keystone.cold.viewmodel.AddAddressViewModel;
 import com.sparrowwallet.hummingbird.registry.CryptoCoinInfo;
 import com.sparrowwallet.hummingbird.registry.CryptoHDKey;
 import com.sparrowwallet.hummingbird.registry.CryptoKeypath;
@@ -18,15 +20,33 @@ public class URRegistryHelper {
 
     public static CryptoHDKey generateCryptoHDKey(String path, int type) {
         byte[] masterFingerprint = Hex.decode(new GetMasterFingerprintCallable().call());
+        ETHAccount ethAccount = AddAddressViewModel.getETHAccount(path);
+        if (path.equals(ETHAccount.BIP44_STANDARD.getPath())) {
+            // both ledger legacy and bip44 standard use M/44'/60'/0
+            path = ETHAccount.LEDGER_LEGACY.getPath();
+        }
         String xPub = new GetExtendedPublicKeyCallable(path).call();
         ExtendedPublicKey extendedPublicKey = new ExtendedPublicKey(xPub);
-        List<PathComponent> pathComponents = getPathComponents(path);
         byte[] key = extendedPublicKey.getKey();
         byte[] chainCode = extendedPublicKey.getChainCode();
         CryptoCoinInfo useInfo = new CryptoCoinInfo(type, 0);
-        CryptoKeypath origin = new CryptoKeypath(pathComponents, masterFingerprint, (int) extendedPublicKey.getDepth());
         byte[] parentFingerprint = extendedPublicKey.getParentFingerprint();
-        return new CryptoHDKey(false, key, chainCode, useInfo, origin, null, parentFingerprint, KEY_NAME, "");
+        CryptoKeypath origin = new CryptoKeypath(getPathComponents(path), masterFingerprint, (int) extendedPublicKey.getDepth());
+        CryptoKeypath children = null;
+        switch (ethAccount) {
+            case LEDGER_LIVE:
+                children = new CryptoKeypath(getPathComponents(path + "/*'/0/0"), masterFingerprint, (int) extendedPublicKey.getDepth());
+                break;
+            case LEDGER_LEGACY:
+                children = new CryptoKeypath(getPathComponents(path + "/*"), masterFingerprint, (int) extendedPublicKey.getDepth());
+                break;
+            case BIP44_STANDARD:
+                children = new CryptoKeypath(getPathComponents(path + "/0/*"), masterFingerprint, (int) extendedPublicKey.getDepth());
+                break;
+            default:
+                break;
+        }
+        return new CryptoHDKey(false, key, chainCode, useInfo, origin, children, parentFingerprint, KEY_NAME, "");
     }
 
     public static List<PathComponent> getPathComponents(String path) {
@@ -38,9 +58,11 @@ public class URRegistryHelper {
                 try {
                     if (item.contains("'")) {
                         item = item.replace("'", "");
-                        pathComponents.add(new PathComponent(Integer.parseInt(item), true));
+                        pathComponents.add(item.equals("*") ? new PathComponent(true) :
+                                new PathComponent(Integer.parseInt(item), true));
                     } else {
-                        pathComponents.add(new PathComponent(Integer.parseInt(item), false));
+                        pathComponents.add(item.equals("*") ? new PathComponent(false) :
+                                new PathComponent(Integer.parseInt(item), false));
                     }
                 } catch (NumberFormatException e) {
                     e.printStackTrace();
