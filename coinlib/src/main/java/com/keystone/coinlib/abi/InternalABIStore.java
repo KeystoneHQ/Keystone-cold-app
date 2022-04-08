19a7f4ae6c3cc1849f7143cbe030d0c3e08604fd
YuZhu <yu@keyst.one>
2022-04-08 14:27:20 +0800

    Handle duplicate abi records

--- coinlib/src/main/java/com/keystone/coinlib/abi/InternalABIStore.java
+++ coinlib/src/main/java/com/keystone/coinlib/abi/InternalABIStore.java
@@ -7,22 +7,27 @@ import org.json.JSONObject;
 
 import static com.keystone.coinlib.v8.ScriptLoader.readAsset;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InternalABIStore implements ABIStoreEngine {
     @Override
-    public Contract load(String address) {
-        Contract contract = new Contract();
+    public List<Contract> load(String address) {
+        List<Contract>  contracts = new ArrayList<>();
         String content = readAsset("abi/" + address + ".json");
         if (!TextUtils.isEmpty(content)) {
             try {
+                Contract contract = new Contract();
                 JSONObject contentJson = new JSONObject(content);
                 JSONObject metadataJson = contentJson.getJSONObject("metadata");
                 JSONObject outputJson = metadataJson.getJSONObject("output");
                 contract.setAbi(outputJson.getString("abi"));
                 contract.setName(contentJson.getString("name"));
+                contracts.add(contract);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
-        return contract;
+        return contracts;
     }
 }
--- coinlib/src/main/java/com/keystone/coinlib/abi/SelfDefinedABIStore.java
+++ coinlib/src/main/java/com/keystone/coinlib/abi/SelfDefinedABIStore.java
@@ -7,26 +7,30 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SelfDefinedABIStore implements ABIStoreEngine {
     private static final String SELF_DEFINE_TFCARD_PATH = "contracts" + File.separator + "self_define";
 
     @Override
-    public Contract load(String address) {
-        Contract contract = new Contract();
+    public List<Contract> load(String address) {
+        List<Contract>  contracts = new ArrayList<>();
         String selfDefineFilePath = SDCardUtil.externalSDCardPath() + File.separator
                 + SELF_DEFINE_TFCARD_PATH + File.separator + address + ".json";
         String content = FileUtil.readFromFile(selfDefineFilePath);
         try {
+            Contract contract = new Contract();
+            contract.setFromTFCard(true);
             JSONObject contentJson = new JSONObject(content);
             JSONObject metadataJson = contentJson.getJSONObject("metadata");
             JSONObject outputJson = metadataJson.getJSONObject("output");
             contract.setAbi(outputJson.getString("abi"));
             contract.setName(contentJson.getString("name"));
+            contracts.add(contract);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        contract.setFromTFCard(true);
-        return contract;
+        return contracts;
     }
 }
