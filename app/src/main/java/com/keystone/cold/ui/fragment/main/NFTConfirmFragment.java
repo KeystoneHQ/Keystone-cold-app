71cd10a4abfe4968731e6ba07171492ce0944d9d
YuZhu <yu@keyst.one>
2022-06-15 10:19:04 +0800

    feat: support solana NFT presentation

--- app/src/main/java/com/keystone/cold/ui/fragment/main/NFTConfirmFragment.java
+++ app/src/main/java/com/keystone/cold/ui/fragment/main/NFTConfirmFragment.java
@@ -4,7 +4,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 
 import androidx.lifecycle.ViewModelProviders;
@@ -16,9 +15,11 @@ import com.keystone.cold.ui.fragment.BaseFragment;
 import com.keystone.cold.ui.modal.ModalDialog;
 import com.keystone.cold.viewmodel.tx.Web3TxViewModel;
 
-import java.util.Arrays;
-
 public class NFTConfirmFragment extends BaseFragment<NftConfirmFragmentBinding> {
+
+
+    public final static String ETH_NFT = "ETH_NFT";
+    public final static String SOL_NFT = "SOL_NFT";
 
     public static String KEY_CHAIN_ID = "CHAIN_ID";
     public static String KEY_CONTRACT_ADDRESS = "CONTRACT_ADDRESS";
@@ -26,11 +27,20 @@ public class NFTConfirmFragment extends BaseFragment<NftConfirmFragmentBinding> 
     public static String KEY_CONTRACT_NAME = "CONTRACT_NAME";
     public static String KEY_MEDIA_DATA = "MEDIA_DATA";
 
+    public static String KEY_MINT_ADDRESS = "MINT_ADDRESS";
+    public static String KEY_COLLECTION_NAME = "COLLECTION_NAME";
+
+    public static String KEY_NFT_TYPE = "NFT_TYPE";
+
     private Integer chainId;
     private String contractAddress;
     private String name;
     private String contractName;
     private String mediaData;
+
+    private String mintAddress;
+    private String collectionName;
+    private String nftType;
 
     private Web3TxViewModel viewModel;
 
@@ -41,22 +51,41 @@ public class NFTConfirmFragment extends BaseFragment<NftConfirmFragmentBinding> 
 
     @Override
     protected void init(View view) {
-        viewModel = ViewModelProviders.of(this).get(Web3TxViewModel.class);
-        chainId = requireArguments().getInt(KEY_CHAIN_ID);
-        contractAddress = requireArguments().getString(KEY_CONTRACT_ADDRESS);
-        contractName = requireArguments().getString(KEY_CONTRACT_NAME);
-        name = requireArguments().getString(KEY_NAME);
-        mediaData = requireArguments().getString(KEY_MEDIA_DATA);
+
+        nftType = requireArguments().getString(KEY_NFT_TYPE);
+
+        if (ETH_NFT.equals(nftType)) {
+            viewModel = ViewModelProviders.of(this).get(Web3TxViewModel.class);
+            chainId = requireArguments().getInt(KEY_CHAIN_ID);
+            contractAddress = requireArguments().getString(KEY_CONTRACT_ADDRESS);
+            contractName = requireArguments().getString(KEY_CONTRACT_NAME);
+            name = requireArguments().getString(KEY_NAME);
+            mediaData = requireArguments().getString(KEY_MEDIA_DATA);
+        } else if (SOL_NFT.equals(nftType)) {
+            mintAddress = requireArguments().getString(KEY_MINT_ADDRESS);
+            collectionName = requireArguments().getString(KEY_COLLECTION_NAME);
+            name = requireArguments().getString(KEY_NAME);
+            mediaData = requireArguments().getString(KEY_MEDIA_DATA);
+        }
     }
 
     @Override
     protected void initData(Bundle savedInstanceState) {
         try {
             mBinding.toolbar.setNavigationOnClickListener(v -> navigateUp());
-            mBinding.network.setText(viewModel.getNetwork(chainId));
-            mBinding.name.setText(name);
-            mBinding.contractName.setText(contractName);
-            mBinding.contractAddress.setText(contractAddress);
+            if (ETH_NFT.equals(nftType)) {
+                mBinding.network.setText(viewModel.getNetwork(chainId));
+                mBinding.name.setText(name);
+                mBinding.contractName.setText(contractName);
+                mBinding.tvAddressName.setText(R.string.import_nft_contract_address);
+                mBinding.contractAddress.setText(contractAddress);
+            } else if (SOL_NFT.equals(nftType)) {
+                mBinding.network.setVisibility(View.GONE);
+                mBinding.name.setText(name);
+                mBinding.contractName.setText(collectionName);
+                mBinding.tvAddressName.setText(R.string.import_nft_mint_address);
+                mBinding.contractAddress.setText(mintAddress);
+            }
 
             mBinding.confirm.setOnClickListener(v -> {
                 Utilities.setNftAvatarResource(mActivity, mediaData);
