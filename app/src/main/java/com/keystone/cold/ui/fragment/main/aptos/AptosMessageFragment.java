5296d60e77aad072bb140960a3634853df2aa028
yu@keyst.one <yu@keyst.one>
2022-10-24 17:18:11 +0800

    feat(aptos): support sign aptos message

---
+++ app/src/main/java/com/keystone/cold/ui/fragment/main/aptos/AptosMessageFragment.java
@@ -0,0 +1,155 @@
+package com.keystone.cold.ui.fragment.main.aptos;
+
+import static com.keystone.cold.callables.FingerprintPolicyCallable.READ;
+import static com.keystone.cold.callables.FingerprintPolicyCallable.TYPE_SIGN_TX;
+import static com.keystone.cold.ui.fragment.main.keystone.BroadcastTxFragment.KEY_SIGNATURE_UR;
+import static com.keystone.cold.ui.fragment.setup.PreImportFragment.ACTION;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.keystone.cold.R;
+import com.keystone.cold.callables.FingerprintPolicyCallable;
+import com.keystone.cold.databinding.AptosSignMessageBinding;
+import com.keystone.cold.ui.fragment.BaseFragment;
+import com.keystone.cold.ui.fragment.setup.PreImportFragment;
+import com.keystone.cold.ui.modal.ModalDialog;
+import com.keystone.cold.ui.modal.SigningDialog;
+import com.keystone.cold.ui.views.AuthenticateModal;
+import com.keystone.cold.util.CharSetUtil;
+import com.keystone.cold.viewmodel.tx.AptosViewModel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.spongycastle.util.encoders.Hex;
+
+import java.nio.charset.StandardCharsets;
+
+public class AptosMessageFragment extends BaseFragment<AptosSignMessageBinding> {
+
+    private AptosViewModel viewModel;
+    private SigningDialog signingDialog;
+
+    private final Runnable forgetPassword = () -> {
+        Bundle bundle = new Bundle();
+        bundle.putString(ACTION, PreImportFragment.ACTION_RESET_PWD);
+        navigate(R.id.action_to_preImportFragment, bundle);
+    };
+
+    @Override
+    protected int setView() {
+        return R.layout.aptos_sign_message;
+    }
+
+    @Override
+    protected void init(View view) {
+        mBinding.toolbar.setNavigationOnClickListener(v -> navigateUp());
+        viewModel = ViewModelProviders.of(this).get(AptosViewModel.class);
+        LiveData<JSONObject> liveData = viewModel.parseRawMessage(requireArguments());
+        liveData.observe(this, o -> onMessageParsed(liveData, o));
+        viewModel.parseTxException().observe(this, this::handleParseException);
+        mBinding.sign.setOnClickListener(v -> handleSign());
+    }
+
+    private void onMessageParsed(LiveData<JSONObject> liveData, JSONObject jsonObject) {
+        if (jsonObject != null) {
+            String message = null;
+            try {
+                message = jsonObject.getString("data");
+                String fromAddress = jsonObject.getString("fromAddress");
+                mBinding.address.setText(fromAddress);
+                if (CharSetUtil.isUTF8Format(Hex.decode(message))) {
+                    String messageUtf8 = new String(Hex.decode(message), StandardCharsets.UTF_8);
+                    mBinding.message.setText(messageUtf8);
+                } else {
+                    mBinding.llMsgUtf8.setVisibility(View.GONE);
+                }
+                mBinding.rawMessage.setText(message);
+                liveData.removeObservers(AptosMessageFragment.this);
+            } catch (UnsupportedOperationException e) {
+                mBinding.llMsgUtf8.setVisibility(View.GONE);
+                mBinding.rawMessage.setText(message);
+                liveData.removeObservers(AptosMessageFragment.this);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                handleParseException(e);
+            }
+        }
+    }
+
+
+    private void handleParseException(Exception ex) {
+        if (ex != null) {
+            ex.printStackTrace();
+            ModalDialog.showCommonModal(mActivity,
+                    getString(R.string.invalid_data),
+                    getString(R.string.incorrect_tx_data),
+                    getString(R.string.confirm),
+                    null);
+            viewModel.parseTxException().setValue(null);
+            popBackStack(R.id.assetFragment, false);
+        }
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+
+    }
+
+    private void handleSign() {
+        boolean fingerprintSignEnable = new FingerprintPolicyCallable(READ, TYPE_SIGN_TX).call();
+        AuthenticateModal.show(mActivity,
+                getString(R.string.password_modal_title), "", fingerprintSignEnable,
+                token -> {
+                    viewModel.setToken(token);
+                    viewModel.handleSignMessage();
+                    subscribeSignState();
+                }, forgetPassword);
+    }
+
+    private void subscribeSignState() {
+        viewModel.getSignState().observe(this, s -> {
+            if (AptosViewModel.STATE_SIGNING.equals(s)) {
+                signingDialog = SigningDialog.newInstance();
+                signingDialog.show(mActivity.getSupportFragmentManager(), "");
+            } else if (AptosViewModel.STATE_SIGN_SUCCESS.equals(s)) {
+                if (signingDialog != null) {
+                    signingDialog.setState(SigningDialog.STATE_SUCCESS);
+                }
+                new Handler().postDelayed(() -> {
+                    if (signingDialog != null) {
+                        signingDialog.dismiss();
+                    }
+                    signingDialog = null;
+                    onSignSuccess();
+                }, 500);
+            } else if (AptosViewModel.STATE_SIGN_FAIL.equals(s)) {
+                if (signingDialog == null) {
+                    signingDialog = SigningDialog.newInstance();
+                    signingDialog.show(mActivity.getSupportFragmentManager(), "");
+                }
+                new Handler().postDelayed(() -> signingDialog.setState(SigningDialog.STATE_FAIL), 1000);
+                new Handler().postDelayed(() -> {
+                    if (signingDialog != null) {
+                        signingDialog.dismiss();
+                    }
+                    signingDialog = null;
+                    viewModel.getSignState().removeObservers(this);
+                }, 2000);
+            }
+        });
+    }
+
+    private void onSignSuccess() {
+        String signature = viewModel.getSignatureUR();
+        Bundle data = new Bundle();
+        data.putString(KEY_SIGNATURE_UR, signature);
+        navigate(R.id.action_to_aptosBroadcastTxFragment, data);
+        viewModel.getSignState().setValue("");
+        viewModel.getSignState().removeObservers(this);
+    }
+}
