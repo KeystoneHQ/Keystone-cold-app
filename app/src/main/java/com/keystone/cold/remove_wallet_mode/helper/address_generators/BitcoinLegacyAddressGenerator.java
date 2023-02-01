2a6ba05a8dc2010d6b76afea5fb9689b02fde2bc
soralit <soralitria@gmail.com>
2023-02-01 14:53:53 +0800

    fix(rwm): btc setup

--- app/src/main/java/com/keystone/cold/remove_wallet_mode/helper/address_generators/BitcoinLegacyAddressGenerator.java
+++ app/src/main/java/com/keystone/cold/remove_wallet_mode/helper/address_generators/BitcoinLegacyAddressGenerator.java
@@ -8,6 +8,10 @@ import com.keystone.cold.db.entity.AddressEntity;
 import com.keystone.cold.remove_wallet_mode.viewmodel.tx.BitcoinTxViewModel;
 
 public class BitcoinLegacyAddressGenerator extends BaseAddressGenerator{
+    public BitcoinLegacyAddressGenerator() {
+        this.coinId = Coins.BTC_LEGACY.coinId();
+    }
+
     @Override
     protected String deriveAddress(int index, AddressEntity addressEntity, AbsDeriver deriver) {
         String path = BitcoinTxViewModel.BTCLegacyPath + "/0/" + index;
