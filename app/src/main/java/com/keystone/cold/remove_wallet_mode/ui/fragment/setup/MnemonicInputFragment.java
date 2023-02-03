a04b6a42118025abcf7b423f54c57e379a1766c3
yu@keyst.one <yu@keyst.one>
2023-02-03 16:16:34 +0800

    fix(rwm_near): add default near account

--- app/src/main/java/com/keystone/cold/remove_wallet_mode/ui/fragment/setup/MnemonicInputFragment.java
+++ app/src/main/java/com/keystone/cold/remove_wallet_mode/ui/fragment/setup/MnemonicInputFragment.java
@@ -47,6 +47,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.navigation.Navigation;
 
 import com.keystone.coinlib.accounts.ETHAccount;
+import com.keystone.coinlib.accounts.NEARAccount;
 import com.keystone.coinlib.accounts.SOLAccount;
 import com.keystone.cold.R;
 import com.keystone.cold.Utilities;
@@ -290,6 +291,7 @@ public class MnemonicInputFragment extends SetupVaultBaseFragment<MnemonicInputF
                 Utilities.setCurrentBelongTo(mActivity, "main");
                 Utilities.setCurrentEthAccount(mActivity, ETHAccount.BIP44_STANDARD.getCode());
                 Utilities.setCurrentSolAccount(mActivity, SOLAccount.SOLFLARE_BIP44.getCode());
+                Utilities.setCurrentNearAccount(mActivity, NEARAccount.MNEMONIC.getCode());
                 Runnable onComplete = () -> {
                     if (dialog != null && dialog.getDialog() != null && dialog.getDialog().isShowing()) {
                         dialog.dismiss();
