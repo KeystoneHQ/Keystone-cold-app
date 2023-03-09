a3bdaec4298538b41e15d53906bb039477e30092
soralit <soralitria@gmail.com>
2023-03-09 10:24:56 +0800

    feat(rwm): adjust bitcoin/ethereum transaction ui

--- app/src/main/java/com/keystone/cold/remove_wallet_mode/ui/fragment/main/tx/ethereum/EthereumConfirmTransactionFragment.java
+++ app/src/main/java/com/keystone/cold/remove_wallet_mode/ui/fragment/main/tx/ethereum/EthereumConfirmTransactionFragment.java
@@ -28,9 +28,8 @@ public class EthereumConfirmTransactionFragment extends ConfirmTransactionFragme
 
     @Override
     protected TabLayoutConfig[] getTabLayouts() {
-        TabLayoutConfig[] layoutConfigs = new TabLayoutConfig[2];
+        TabLayoutConfig[] layoutConfigs = new TabLayoutConfig[1];
         layoutConfigs[0] = new TabLayoutConfig(getString(R.string.overview), EthereumTransactionDetailFragment.newInstance(requireArguments(), transaction));
-        layoutConfigs[1] = new TabLayoutConfig(getString(R.string.raw_data), RawTxFragment.newInstance(requireArguments(), viewModel.getRawFormatTx()));
         return layoutConfigs;
     }
 
